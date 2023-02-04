bd07a4d8d90b4efbbbba9e54dd4634cf13cb506f
Peter Murín <peter.murin@kios.sk>
2023-02-04 17:08:57 +0100

    Project rename.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ToDo-App/",
+  base: "/To-Do-App/",
   plugins: [eslintPlugin(), vue()],
 });
